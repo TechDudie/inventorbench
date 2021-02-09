79ceac8f8e4c43661167240bccba24f568645cd5
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-02-09 08:56:41 -0600

    v0.0.0

---
+++ init.lua
@@ -0,0 +1,42 @@
+dofile(minetest.get_modpath("inventorbench") .. "/tools.lua")
+minetest.register_craft({
+    output = "inventorbench:bench",
+    recipe = {
+        {"default:wood", "default:steel_ingot", "default:wood"},
+        {"default:wood", "default:wood", "default:wood"},
+        {"default:wood", "default:wood", "default:wood"}
+    }
+})
+minetest.register_node("inventorbench:bench", {
+	description = "Inventor Bench",
+	tile_images = {"inventor_bench.png"},
+	is_ground_content = true,
+	groups = {oddly_breakable_by_hand=1},
+	sounds = default.node_sound_stone_defaults(),
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("formspec",
+				"invsize[8,9;]"..
+				"label[0,0;Inventor Bench]"..
+				"list[current_name;main;4,1;3,3;]"..
+				"list[current_player;main;0,5;8,4;]")
+		meta:set_string("infotext", "Inventor Bench")
+		local inv = meta:get_inventory()
+		inv:set_size("main", 3*3)
+	end,
+})
+minetest.register_abm({
+    nodenames = {"inventorbench:bench"},
+    neighbors = {"anvil:anvil", "default:chest", "default:furnace", "default:furnace_active"},
+    interval = 1,
+    chance = 10,
+    action = function(pos, node, active_object_count, active_object_count_wider)
+	local meta = minetest.get_meta(pos)
+	local inv = meta:get_inventory()
+	local invlist = inv:get_list("main")
+	if invlist = {"default:steel_ingot 3", "default:stick 2"} then
+		inv:remove_item("default:steel_ingot 3")
+		inv:remove_item("default:stick 2")
+		inv:add_item("inventor-bench:stairpick")
+    end
+})
---
+++ tools.lua
@@ -0,0 +1,4 @@
+minetest.register_craftitem("inventorbench:stairpick", {
+    description = "Stairpick",
+    inventory_image = "stairpick.png"
+})
